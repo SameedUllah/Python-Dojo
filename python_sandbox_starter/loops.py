be575fd1744dff83c8b45c0a3e8acaa6fb0f9117
sameedullah <sameedu786@gmail.com>
2020-11-07 12:42:21 +0500

    Half Done

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -25,7 +25,12 @@ people = ['john', 'paul','sara','william']
 # for i in range(len(people)):
 #     print(people[i])
 
-for i in range (0,11):
-    print(f'the number is {i}')
+# for i in range (0,11):
+#     print(f'the number is {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+
+count = 0
+while count <=10:
+    print(f'Count {count}')
+    count +=1

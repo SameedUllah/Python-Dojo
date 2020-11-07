a6934746e4ff5780c01c574709e416addf321f80
sameedullah <sameedu786@gmail.com>
2020-11-07 12:10:16 +0500

    Half Done

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,39 +1,43 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
-x = 10
+x = 3
 y = 20
 
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 # elif
-if x>y:
-    print(f'{x} is greater than {y}')
-elif x == y:
-    print(f'{x} is equal to {y}')
-else:
-    print(f'{y} is greater than {x}')
+# if x>y:
+#     print(f'{x} is greater than {y}')
+# elif x == y:
+#     print(f'{x} is equal to {y}')
+# else:
+#     print(f'{y} is greater than {x}')
 
 # Nested if statement
-if x > 2:
-    if x <= 10:
-        print(f'{x} is greater than 2 and less than or equal to 10')
+# if x > 2:
+#     if x <= 10:
+#         print(f'{x} is greater than 2 and less than or equal to 10')
 
 
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
 # And
-if x > 2 and x <= 10:
-    print(f'{x} is greater than 2 and less than or equal to 10')
+# if x > 2 and x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10')
 
-# Or
-if x > 2 or x <= 10:
-    print(f'{x} is greater than 2 and less than or equal to 10')
+# # Or
+# if x > 2 or x <= 10:
+#     print(f'{x} is greater than 2 and less than or equal to 10')
 
-# Not
-if x > 2 not x<=10
+# # Not
+# if not(x == y) :
+#     print(f'{x} is not equal to {y}')
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
+numbers = [1,2,3,4,5,6]
 
+if x in numbers:
+    print(x in numbers)
 
 
 
